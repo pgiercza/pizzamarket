35e92aaf022567e64e717ba8a6a0eed53a724f86
Piotrek <pgiercza@gmail.com>
2022-05-14 11:14:17 +0200

    Konfiguracja projektu - encja

---
+++ src/main/java/pl/wszib/pizzamarket/data/entities/PizzaEntity.java
@@ -0,0 +1,55 @@
+package pl.wszib.pizzamarket.data.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name="pizzas")
+public class PizzaEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+    @Column(name="name")
+    private String name;
+    @Column(name="price")
+    private BigDecimal price;
+    @ManyToMany
+    @JoinTable(name = "pizza_ingredients",joinColumns = @JoinColumn(name = "pizza_id"),
+    inverseJoinColumns = @JoinColumn(name="ingredient_id"))
+
+    private Set<IngredientsEntity> ingredients;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Set<IngredientsEntity> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<IngredientsEntity> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
