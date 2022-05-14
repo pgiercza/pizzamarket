993c561260a3be514b565aeba58a4b0e1672b470
Piotrek <pgiercza@gmail.com>
2022-05-14 12:01:30 +0200

    Tworzenie encji

---
+++ src/main/java/pl/wszib/pizzamarket/data/entity/OrderEntity.java
@@ -0,0 +1,42 @@
+package pl.wszib.pizzamarket.data.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "")
+    private Long id;
+    @Column(name = "pizza_name")
+    private String pizzaName;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_adress_id")
+    private OrderAdresEntity orderAdress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPizzaName() {
+        return pizzaName;
+    }
+
+    public void setPizzaName(String pizzaName) {
+        this.pizzaName = pizzaName;
+    }
+
+    public OrderAdresEntity getOrderAdress() {
+        return orderAdress;
+    }
+
+    public void setOrderAdress(OrderAdresEntity orderAdress) {
+        this.orderAdress = orderAdress;
+    }
+}
